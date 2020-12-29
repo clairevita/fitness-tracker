a7f1c76819c3bd40c3808808707c8dbb61c232ff
Claire Vita <clairej.vita@gmail.com>
2020-12-28 22:46:13 -0500

    model parameters pseudo

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    
+    //type
+    //weight:
+        //name
+        //weight
+        //sets
+        //reps
+        //resistance
+    //cardio:
+        //name
+        //duration
+        //distance
+
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
