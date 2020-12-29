17186a2af07297a53a47181522a8e454a68b73e6
Claire Vita <clairej.vita@gmail.com>
2020-12-28 22:16:30 -0500

    fixed server.js formatting

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-  router.get("/", (req, res) => {
-  
-  });
-
-  router.get("/stats", (req, res) => {
-    
-  });
--- server.js
+++ server.js
@@ -6,18 +6,24 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true
+const URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
-app.use(require("./routes/routes.js"));
-
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
