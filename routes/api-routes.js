ffc859147f719bccd8413413b966b98e75cc5250
Claire Vita <clairej.vita@gmail.com>
2021-01-08 21:23:19 -0500

    aggregation

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,7 @@ const db = require("../models");
 
 router.get("/api/workouts", (req, res)=>{
     db.Workout.find({})
-    .sort({ date: -1 })
+    .sort({ date: 1 })
     .then(results=>{
         res.json(results);
     })
@@ -19,7 +19,7 @@ router.get("/api/workouts", (req, res)=>{
 
 router.put("/api/workouts/:id",( req, res)=>{
     db.Workout.findByIdAndUpdate(
-        req.params.id, 
+        {_id: req.params.id}, 
         {$push: {exercises: req.body}
     })
     .then(results=>{
@@ -42,7 +42,8 @@ router.post("/api/workouts", (req, res)=>{
 
 router.get("/api/workouts/range", (req, res)=>{
     db.Workout.find({})
-    .sort({ date: -1 })
+    .limit(7)
+    .sort({ day: 1 })
     .then(results=>{
         res.json(results);
     })
